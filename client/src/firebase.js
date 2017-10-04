c90101b42e380071bb42c746d4a440e0756cd894
ajliss <aaron@lissismore.com>
2017-10-04 10:12:59 -0700

    Add own firebase configurations.

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,12 +2,18 @@ import firebase from 'firebase'
 
 // Initialize Firebase
 const config = {
-  apiKey: "AIzaSyC2rf7NT_vVD9E7-CFAPAsLExJG0IPGxf8",
-  authDomain: "trove-app.firebaseapp.com",
-  databaseURL: "https://trove-app.firebaseio.com",
-  projectId: "trove-app",
-  storageBucket: "trove-app.appspot.com",
-  messagingSenderId: "50556960736"
+  // apiKey: "AIzaSyC2rf7NT_vVD9E7-CFAPAsLExJG0IPGxf8",
+  // authDomain: "trove-app.firebaseapp.com",
+  // databaseURL: "https://trove-app.firebaseio.com",
+  // projectId: "trove-app",
+  // storageBucket: "trove-app.appspot.com",
+  // messagingSenderId: "50556960736"
+  apiKey: "AIzaSyBk9XOyCP38nIH6gfdnOUN2lFB_wHD1EHI",
+  authDomain: "troveplusplus.firebaseapp.com",
+  databaseURL: "https://troveplusplus.firebaseio.com",
+  projectId: "troveplusplus",
+  storageBucket: "",
+  messagingSenderId: "118710711934"
 }; 
 
 firebase.initializeApp(config);
